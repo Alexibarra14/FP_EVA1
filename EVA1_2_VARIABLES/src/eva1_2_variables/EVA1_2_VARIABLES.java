0bb9c9ec80551b9f7dfd100e67ec76779be73196
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2024-09-04 12:22:57 -0700

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variables;
+
+/**
+ *
+ * @author brend
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //TIPOS DE DATOS
+        
+      System.out.println("hola mundo");// cadena de texto
+      System.out.println(100);         // Entero
+      System.out.println(3.1416);      //Numero con punto flotante
+      System.out.println('c');         //caracter
+      System.out.println(true);     // Valores logicos
+      System.out.println
+        (false)
+        ;// Fin de la instruccion
+      
+      //Tipos de datos en Java
+      //Enteros
+      //Int
+      //long
+      //Short
+      
+      //DECIMAL
+      //Float
+      //double
+      
+      //Cadena de texto
+      //string
+      
+      //CARACTERES
+      //Char
+      
+      //Valores logicos
+      //Boolean
+      
+      //Declaracion de variables
+      // Sintaxis o reglas
+      //Tipo de dato, nombre de la variable y;
+      String nombre;
+      String apellido;
+      String curp;
+      
+      double promedio;
+      int edad;
+      
+      edad= 18; //Asignacion: Coloque el 18 dentro de la variable
+      
+      nombre = "Alejandro";
+      apellido = "ibarra valdez";
+      promedio = 1.0;
+      System.out.println(edad);
+        System.out.println(nombre);
+          System.out.println(apellido);
+            System.out.println(promedio);
+            
+    }
+    
+}
