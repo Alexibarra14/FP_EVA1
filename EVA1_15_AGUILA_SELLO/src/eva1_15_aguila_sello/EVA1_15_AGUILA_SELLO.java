b668d4a6193313b71e47049aaab8d9e8349da08f
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2024-09-11 11:55:04 -0700

    Add files via upload

---
+++ EVA1_15_AGUILA_SELLO/src/eva1_15_aguila_sello/EVA1_15_AGUILA_SELLO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_15_aguila_sello;
+
+/**
+ *
+ * @author brend
+ */
+public class EVA1_15_AGUILA_SELLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double valor= Math.random();
+      
+        
+        if(valor<=0.5){
+            System.out.println("AGUILA");
+            
+             }else {
+            System.out.println("SELLO");
+            }
+    
+    
+        }
+    }
+    
+
